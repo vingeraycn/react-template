dc1d38e4b13cac3ed2e0691812b2b4e0a3847a7e
VingeRay <vingeray@VingeRaydeiMac.local>
2021-07-18 23:15:51 +0800

    chore: bump deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Index from './pages';
 
 const App = () => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const Block = styled.div`
