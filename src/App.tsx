b9fa1fd0e32db182be8c81b330e933a570fb6e45
leihancn <vingeray@gmail.com>
2022-01-14 15:54:15 +0800

    refactor: 💡 clean code

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,11 @@ function App() {
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-            count is:
-            {' '}
-            {count}
+            count is: {count}
           </button>
         </p>
         <p>
-          Edit
-          {' '}
-          <code>App.tsx</code>
-          {' '}
-          and save to test HMR updates.
+          Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
