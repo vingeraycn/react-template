3ce1202b1183a17b1135ee2beaaacd3ac0202d91
雷玮杰 <vingeray@gmail.com>
2022-02-08 07:14:24 +0000

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'error',
   },
   settings: {
     react: {
