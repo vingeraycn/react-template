ea57c3fe61ba6d9f8262b0d2ecf1fd0d5b6281ea
雷玮杰 <vingeray@gmail.com>
2022-04-27 03:13:31 +0000

    refactor: 💡 use @vitejs/plugin-react insteadof plugin-react-ref

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   esbuild: {
     jsxInject: "import React from 'react'",
   },
